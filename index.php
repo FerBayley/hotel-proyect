de044e639b87649cb6170fb6d2d65654efff9702
Fernando <fernando.bayley@gmail.com>
2016-06-27 11:37:35 -0300

    Primer commit guardo proyecto

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<head>
+	<title>DosPuntoClick - Diseño y desarrollo web prepago</title>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="public/css/normalize.css" />
+	<link rel="stylesheet" href="public/css/main.css" />
+</head>
+<body>
+
+<section class="Contenedor-central">	
+	<header>
+		header
+	</header> <!--/header-->
+
+	<section class="Texto-explicacion">
+		<article>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</article>
+	</section> <!--/Texto-explicacion -->
+
+	<section class="Texto-servicios">
+		<h2>Texto explicativo para los servicios</h2>
+		<article>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</article>
+	</section> <!--/Texto-servicios -->
+
+	<section class="Muestra-clientes">
+		Logos de nuestros clientes, actuales y pasados
+	</section> <!--/Muestra-clientes -->
+
+	<section class="Costumer-profile">
+		perfiles de nuestros clientes
+	</section> <!--/Costumer-profile -->
+
+	<section class="Metrics-panel">
+		Metricas
+	</section> <!--/Metrics-panel -->
+
+	<section class="Pauta">
+		<div class="Pauta-col1">
+			<h2>Nesecito mas exposicion de mi serivios</h2>
+			<h3>Desarrollamos tu estrategia en redes sociales</h3>
+			<div class="Cuadro-celeste">
+				<a href="#">VER MAS</a>
+			</div> <!--/Cuadro-celeste -->
+		</div> <!--/Pauta-col1 -->
+
+		<div class="Pauta-col2"></div> <!--/Pauta-col2 -->
+	</section> <!--/Pauta -->
+
+	<section class="Pauta-emailMarketing">
+		<div class="Cuadro">
+			<img src="public/images/email-marketing.png" height="150px" width="150px" alt="Email Marketing" >
+			<article>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.
+				</p>
+				<a href="#">VER MAS</a>
+			</article>
+			<h6>*Este servicio no esta incluido dentro de la tarifa prepaga.</h6>
+		</div> <!--/Cuadro -->
+	</section> <!--/Pauta-emailMarketing -->
+
+	<footer>
+		<section class="Info">
+			<article>
+				<p>&copy; 2PuntoClick.com │ Todos los derechos reservados 2016.</p>
+				<p><a href="#">contacto@dospunbtoclick.com</a> │ <a href="#">Terminos y condiciones del servicio</a></p>
+				<p>Buenos Aires - Republica Argentina</p>
+			</article>
+		</section> <!--/Info -->
+
+		<section class="Redes">
+			<ul>
+				<li><a href="#"><img src="public/images/facebook.png" width="40px" height="40px" alt="Facebook"></a></li>
+				<li><a href="#"><img src="public/images/twitter.png" width="40px" height="40px" alt="Twitter"></a></li>
+				<li><a href="#"><img src="public/images/instagram.png" width="40px" height="40px" alt="Instagram"></a></li>
+			</ul>
+		</section> <!--/ Redes -->
+	</footer> <!--/Footer -->
+
+	<section class="Loving-footer">
+		<ul>
+			<li>Dedicado a Raúl Marengo</li>
+			<li><img src="public/images/heart.png" width="20px" height="20px" alt="Corazon"></li>
+		</ul>
+	</section> <!--/Loving-footer -->
+
+</section> <!--/Contenedor-central-->
+
+</body>
+</html>
