24840c4e4fac8c5041491b5cb6b399a895a9adfa
Fernando <fernando.bayley@gmail.com>
2016-07-09 16:23:29 -0300

    Se finalizo la imagen proncipal con call to action

--- index.php
+++ index.php
@@ -4,25 +4,58 @@
 <head>
 	<title>DosPuntoClick - Diseño y desarrollo web prepago</title>
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="public/css/normalize.css" />
 	<link rel="stylesheet" href="public/css/main.css" />
 </head>
 <body>
 
 <section class="Contenedor-central">	
-	<header>
-		header
-	</header> <!--/header-->
+	
+	<section class="Contenedor-principal">
+		<header>
+			<div class="Nombre">
+				<h1>2PuntoClick</h1>
+				<h2>WEB PRODUCTS DESIGNERS</h2>
+			</div> <!--/Nombre -->
+
+			<nav class="Navegacion">
+				<ul>
+					<li><a href="#">NOSOTROS</a> |</li>
+					<li><a href="#">SERVICIOS</a> |</li>
+					<li><a href="#">TRABAJA EN 2.C</a> |</li>
+					<li><a href="#">contacto@dospuntoclick.com.ar</a></li>
+				</ul>
+			</nav> <!--/nav / Navegacion -->
+
+			<div class="Texto-destacado">
+				<p>Tu unidad de negocios en la web</p>
+				<h2>Pensamos | Diseñamos | Desarrollomos | Alojamos | Medimos y analizamos tu empresa en la web | Soporte tecnico</h2>
+				<h3>A solo ar$990 x mes</h3>
+			</div> <!--/Texto-destacado -->
+
+			<section class="Botonera">
+				<a href="#">
+					<div class="Btn1">
+						CONTACTANOS
+					</div> <!--/Btn1 -->
+				</a>
+
+				<a href="#">
+					<div class="Btn2">
+						CONTRATAR
+					</div> <!--/Btn2 -->
+				</a>
+			</section> <!--/Botonera -->
+		</header> <!--/header -->
+
+	</section> <!--/Contenedor-principal -->
 
 	<section class="Texto-explicacion">
 		<article>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Somos un grupo de profesionales netrenados en las artes del diseño y el desarrollo web, con años de experiencia en la industria. Pensando, doseñanado y desarrollando sitios web que funcionan, escalan y se pueden medir, logrando que el clientetenga una verdadera unidad de negocios en linea, que le permita incrementar sus ventas, fidelizar a sus clientes y lograr posicionar mejor su marca.
 			</p>
 		</article>
 	</section> <!--/Texto-explicacion -->
@@ -49,14 +82,18 @@
 		perfiles de nuestros clientes
 	</section> <!--/Costumer-profile -->
 
+	<section class="Ceo-profile">
+		Datos del CEO
+	</section> <!--/Ceo-profile -->
+
 	<section class="Metrics-panel">
 		Metricas
 	</section> <!--/Metrics-panel -->
 
 	<section class="Pauta">
 		<div class="Pauta-col1">
-			<h2>Nesecito mas exposicion de mi serivios</h2>
-			<h3>Desarrollamos tu estrategia en redes sociales</h3>
+			<h2>Tengo que dar a conocer mis serivios en las redes</h2>
+			<h3>Desarrollamos tu estrategia de marketing on line</h3>
 			<div class="Cuadro-celeste">
 				<a href="#">VER MAS</a>
 			</div> <!--/Cuadro-celeste -->
@@ -77,7 +114,7 @@
 				</p>
 				<a href="#">VER MAS</a>
 			</article>
-			<h6>*Este servicio no esta incluido dentro de la tarifa prepaga.</h6>
+			<h3>*Este servicio no esta incluido dentro de la tarifa prepaga.</h3>
 		</div> <!--/Cuadro -->
 	</section> <!--/Pauta-emailMarketing -->
 
