0ec8f56b9d95773b8d106b66194aff28d0b9aa4a
Fernando <fernando.bayley@gmail.com>
2016-07-14 20:26:41 -0300

    Pagina principal finalizada sin detalles

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>DosPuntoClick - Diseño y desarrollo web prepago</title>
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="public/css/normalize.css" />
 	<link rel="stylesheet" href="public/css/main.css" />
@@ -22,16 +23,16 @@
 
 			<nav class="Navegacion">
 				<ul>
-					<li><a href="#">NOSOTROS</a> | </li>
-					<li><a href="#">SERVICIOS</a> | </li>
-					<li><a href="#">TRABAJA EN 2.C</a> | </li>
+					<li><a href="#">NOSOTROS</a></li>
+					<li><a href="#">SERVICIOS</a></li>
+					<li><a href="#">TRABAJA EN 2.C</a></li>
 					<li><a href="#">CONTACTO</a></li>
 				</ul>
 			</nav> <!--/nav / Navegacion -->
 
 			<div class="Texto-destacado">
 				<p>Tu unidad de negocios en la web</p>
-				<h2>Pensamos | Diseñamos | Desarrollomos | Alojamos | Medimos y analizamos tu empresa en la web | Soporte tecnico</h2>
+				<h2>Pensamos | Diseñamos | Desarrollomos | Alojamos | Medimos y analizamos tu empresa en la web | + Soporte tecnico</h2>
 				<h3>A solo ar$990 x mes</h3>
 			</div> <!--/Texto-destacado -->
 
@@ -55,45 +56,119 @@
 	<section class="Texto-explicacion">
 		<article>
 			<p>
-				Somos un grupo de profesionales netrenados en las artes del diseño y el desarrollo web, con años de experiencia en la industria. Pensando, doseñanado y desarrollando sitios web que funcionan, escalan y se pueden medir, logrando que el clientetenga una verdadera unidad de negocios en linea, que le permita incrementar sus ventas, fidelizar a sus clientes y lograr posicionar mejor su marca.
+				Somos un grupo de profesionales entrenados en las artes del <span class="Resaltado">diseño y el desarrollo web</span>, con años de experiencia en la industria. 
+			</p>
+			<p>
+				Pensando, diseñanado y desarrollando sitios web que <span class="Resaltado">funcionan, escalan y se pueden medir</span>, logrando que el cliente tenga una verdadera unidad de negocios en linea, que le permita <span class="Resaltado">incrementar sus ventas, fidelizar a sus clientes y lograr un posicionar mejor su marca.
+				</span>
 			</p>
 		</article>
+
+		<a href="">
+			<div class="UnicoPLan">
+				<p>En un unico plan todos los servicios - VER MAS</p>
+			</div> <!--/UnicoPLan -->
+		</a>
 	</section> <!--/Texto-explicacion -->
 
-	<section class="Texto-servicios">
-		<h2>Texto explicativo para los servicios</h2>
-		<article>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-		</article>
-	</section> <!--/Texto-servicios -->
+	<section class="D-yD"></section> <!--/D-yD -->
 
 	<section class="Muestra-clientes">
-		Logos de nuestros clientes, actuales y pasados
+		<ul>
+			<li><img src="public/images/brand/jonnie.jpg" width="144px" height="85px" alt="Johnnie Walker" ></li>
+			<li><img src="public/images/brand/juana.jpg" width="180px" height="66px" alt="Juana M Restoran" ></li>
+			<li><img src="public/images/brand/inoxigas.jpg" width="130px" height="116px" alt="Inoxigas Argentina" ></li>
+			<li><img src="public/images/brand/ingloba.jpg" alt="Ingloba Web" ></li>
+			<li><img src="public/images/brand/placcos.jpg" alt="Placcos Muebles a medida" ></li>
+			<li><img src="public/images/brand/arquinova.jpg" width="95px" height="89px" alt="Arquinova Color" ></li>
+		</ul>
 	</section> <!--/Muestra-clientes -->
 
 	<section class="Costumer-profile">
-		perfiles de nuestros clientes
+		<section class="Clientes">
+			<div class="Clientes1">
+				<figure><img src="public/images/profile/flabia.jpg" width="140px" height="140px" alt="Flabia Vit" /></figure>
+				<h2>Lic.Flabia Vit</h2>
+				<a href="mailto:info@flabiavit.com.ar">info@flabiavit.com.ar</a>
+					<article>
+						<p>
+							"Desde hace mas de 10 años trabajamos con la agencia en todos nuestros productos para la web, en este tiempo tuvimos importantes resultados que llevaron a otro nivel nuestra relacion con los pacientes."
+						</p>
+					</article>
+					<a href="http://www.flabiavit.com.ar">www.flabiavit.com.ar</a>
+			</div> <!--/Clientes1 -->
+
+			<div class="Clientes2">
+				<figure><img src="public/images/profile/juana.png" width="140px" height="140px" alt="Kike" /></figure>
+				<h2>Quique Geisele</h2>
+				<a href="mailto:info@juanam.com">info@juanam.com</a>
+					<article>
+						<p>
+							"Dia a dia tenemos gran cantidad de reservas a traves de nuestro web site, logrando una gran base de comensales, los cuales contactamos mes a mes con beneficios exclusivos desde nuestro news letter."
+						</p>
+					</article>
+					<a href="http://www.juanam.com">www.juanam.com</a>
+			</div> <!--/Clientes2 -->
+
+			<div class="Clientes3">
+				<figure><img src="public/images/profile/arqui.png" width="140px" height="140px" alt="Mariano Perez" /></figure>
+				<h2>Mariano Perez</h2>
+				<a href="mailto:marianop@arquinovacolor.com.ar">marianop@arquinovacolor.com.ar</a>
+					<article>
+						<p>
+							"Nuestro mayor feature es nuestro chat, nos conecta al instante con nuestros clientes y lo mejor de todo, con nuestros nuevos clientes, una simple idea nos hizo posicionarnos mejor que la competencia."
+						</p>
+					</article>
+					<a href="http://www.arquinovacolor.com.ar">www.arquinovacolor.com.ar</a>
+			</div> <!--/Clientes3 -->
+		</section> <!--/Clientes -->
 	</section> <!--/Costumer-profile -->
 
-	<section class="Ceo-profile">
-		Datos del CEO
-	</section> <!--/Ceo-profile -->
+	<section class="Services">
+		<div class="Services1">
+			<h2>Diseño y desarrollo</h2>
+			<img src="public/images/dev-dis.png" width="170px" height="170px" alt="Diseño y desarrollo">
+			<br>
+				<a href="#">ver mas</a>
+		</div> <!--/Services1 -->
 
-	<section class="Metrics-panel">
-		Metricas
-	</section> <!--/Metrics-panel -->
+		<div class="Services2">
+			<h2>Alojamiento</h2>
+			<img src="public/images/alojamiento.png" width="170px" height="170px" alt="Alojamiento web">
+			<br>
+				<a href="#">ver mas</a>
+		</div> <!--/Services2 -->
+
+		<div class="Services3">
+			<h2>Metricas</h2>
+			<img src="public/images/medimos.png" width="170px" height="170px" alt="Metricas y consultoria">
+			<br>
+				<a href="#">ver mas</a>
+		</div> <!--/Services3 -->
+
+		<div class="Services4">
+			<h2>Soporte</h2>
+			<img src="public/images/soporte.png" width="170px" height="170px" alt="Soporte tecnico">
+			<br>
+				<a href="#">ver mas</a>
+		</div> <!--/Services4 -->
+
+		<div class="Services5">
+			<h2>UpGrade</h2>
+			<img src="public/images/rocket.png" width="170px" height="170px" alt="Upgrade de servicios">
+			<br>
+				<a href="#">ver mas</a>
+		</div> <!--/Services5 -->
+	</section> <!--/Services -->
+
+	<section class="Montain">
+		<h2>Conoce nuestra metodologia de trabajo #TeamEverest</h2>
+	</section> <!--/Montain -->
 
 	<section class="Pauta">
 		<div class="Pauta-col1">
 			<h2>Tengo que dar a conocer mis serivios en las redes</h2>
-			<h3>Desarrollamos tu estrategia de marketing on line</h3>
+			<h3>Desarrollamos tu estrategia de marketing digital</h3>
 			<div class="Cuadro-celeste">
 				<a href="#">VER MAS</a>
 			</div> <!--/Cuadro-celeste -->
@@ -110,7 +185,9 @@
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat.
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 				<a href="#">VER MAS</a>
 			</article>
