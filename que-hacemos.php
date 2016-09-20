57ff4f974d97434993c671e76362a1e755794c8f
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2016-09-20 10:02:57 -0300

    Cambios en seccion que hacemos

---
+++ que-hacemos.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+<meta charset="utf-8">
+<?php include_once("includes/analyticstracking.php") ?>
+<head>
+	<title>DosPuntoClick Diseño y desarrollo web prepago - Que hacemos en 2.Click</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Fredoka+One|Open+Sans+Condensed:300|Open+Sans:300" rel="stylesheet">
+	<link rel="stylesheet" href="public/css/normalize.css" />
+	<link rel="stylesheet" href="public/css/main.css" />
+</head>
+<body>
+
+<section class="Contenedor-central">
+
+	<?php include("includes/header.php"); ?>
+	
+	<section class="Contenedor">
+		<article class="Contenedor-Copy">
+			<h2 class="Contenedor-titulo">Que hacemos en 2.Click?</h2>
+		</article>
+	</section> <!-- / Contenedor -->
+
+	<section class="Estructura">
+		<div class="Estructura-1">
+			<img src="public/images/que-hacemos.jpg" width="600px" height="400px" alt="Fernando Bayley">
+		</div> <!-- / Estructura-1 -->
+
+		<div class="Estructura-2">
+			<article>
+				<p>
+					En 2.Click hacemos sitios web, ahora, no solo es un simple sitio, es una unidad de nogocios que esta on line las 24hs. los 365 dias del año, trabajando y emitiendo datos todo el tiempo.
+				</p>
+				<p>
+					En la agencia elaboramos desde cero tus ideas y la llevamos a la realidad utilizando las tecnologias que el mundo usa.
+				</p>
+				<p>
+					Trabajamos con la modalidad de un servicio prepago mensual, haciendo sitios totalemnte a medidia, que se adaptan a cualquier pantalla de cualqueir dispositivo movil y son totalmente autoadministrables.
+				</p>
+			</article>
+		</div><!-- / Estructura-2 -->
+	</section> <!-- / Seo-estructure -->
+
+	<?php include("includes/footer.php"); ?>
+
+</section> <!--/Contenedor-central-->
+
+<script src="public/js/script.js"></script>
+</body>
+</html>
